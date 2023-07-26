c9c3ac7791eec93ad9f95a3611ae318b4de62457
오현화 <meke@nate.com>
2023-07-26 15:08:12 +0900

    Add proxy pattern concrete proxy class

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_proxy.ConcreteProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -8,8 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
 //@Import(AppV2Config.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
